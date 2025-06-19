8eb4bb44c94b37af837636c10dc5b6ff9f9b3acb
PipoyD <112020835+PipoyD@users.noreply.github.com>
2025-06-19 22:48:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
-require('dotenv').config();
-
-const express = require("express");Add commentMore actions
-const path = require("path");
-
-const mongoose = require('mongoose');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname)));Add commentMore actions
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "register.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Zone TSADE is running on port ${PORT}`);
-});
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('✅ MongoDB connecté'))
-.catch(err => console.error('❌ Erreur MongoDB:', err));
